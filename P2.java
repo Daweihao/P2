449b26e4a1e06504230a00b343dbb7375ccb6c32
davidhao <421679009@qq.com>
2016-09-19 04:16:58 +0530

    visitor checking in undifined , type  and Uninitialized errors ,also add in P2.java file

--- P2.java
+++ P2.java
@@ -1,15 +1,53 @@
+import symboltable.WClasses;
 import syntaxtree.Node;
-import visitor.GJNoArguDepthFirst;
+import utils.PrintError;
+import visitor.ChInitVisitor;
+import visitor.ChTypeErrorVisitor;
+import visitor.CheckUdfVisitor;
+import visitor.SyTableBuilderVisitor;
 
 public class P2 {
    public static void main(String [] args) {
       try {
          Node root = new MiniJavaParser(System.in).Goal();
          System.out.println("Program parsed successfully");
-       root.accept(new GJNoArguDepthFirst()); // Your assignment part is invoked here.
+         WClasses w_classes = new WClasses();
+
+       root.accept(new SyTableBuilderVisitor(),w_classes); // Your assignment part is invoked here.
+         if (PrintError.hasError()){
+            PrintError.printAll();
+            return;
+         }
+         w_classes.update();
+         w_classes.checkLoop();
+         w_classes.checkOverride();
+         w_classes.checkType();
+         if (PrintError.hasError()){
+            PrintError.printAll();
+            return;
+         }
+
+         root.accept(new CheckUdfVisitor(),w_classes);
+         if (PrintError.hasError()){
+            PrintError.printAll();
+            return;
+         }
+
+         root.accept(new ChTypeErrorVisitor(),w_classes);
+         root.accept(new ChInitVisitor(),w_classes);
+         if (PrintError.hasError()){
+            PrintError.printAll();
+         }
+
+      }
+      catch (TokenMgrError e){
+          e.printStackTrace();
       }
       catch (ParseException e) {
          System.out.println(e.toString());
+      }
+      catch (Exception e){
+          e.printStackTrace();
       }
    }
 } 
